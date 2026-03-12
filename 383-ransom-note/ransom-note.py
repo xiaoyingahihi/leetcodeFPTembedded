0a12af657b14544951a729a88da421a744868fa3
vinhphamckd <143116909+xiaoyingahihi@users.noreply.github.com>
2026-03-12 14:57:09 +0700

    Time: 7 ms (96.79%) | Memory: 12.6 MB (96.91%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        for c in ransomNote: 
+            if c in magazine:
+                magazine = magazine.replace(c,'',1)
+            else: return False
+        return True
+        
