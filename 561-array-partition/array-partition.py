4e177a3e587a7e4732c4d35f146fa203b787bd2e
vinhphamckd <143116909+xiaoyingahihi@users.noreply.github.com>
2026-03-20 21:02:29 +0700

    Time: 43 ms (21.46%) | Memory: 13.8 MB (93.91%) - LeetSync

---
+++ 561-array-partition/array-partition.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        nums.sort()
+        result = 0
+        for i in range(0, len(nums), 2):
+            result += nums[i]
+        return result
+        
